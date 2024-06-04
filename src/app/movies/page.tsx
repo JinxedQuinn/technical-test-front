ecafadd454e39345e6a9428130128880895e1960
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-06-04 09:07:20 +0200

    Started Header

---
+++ src/app/movies/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Page = (props: SideProjectsProps) => {
+  return (
+    <main>
+        <div className="bg-yellow-300 text-center p-6 text-3xl">
+            MOVIE NIGHT
+        </div>
+    </main>
+  );
+};
+
+export default Page;
